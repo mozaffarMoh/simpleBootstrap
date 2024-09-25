191b368089ced6e039e9abe940777127df3760d4
Mozaffar Mohammad <134216611+mozaffarMoh@users.noreply.github.com>
2024-09-25 23:01:36 +0300

    enhance-1

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const screenWidth = document.getElementById("screen-width");
+
+
+screenWidth.style.fontFamily = "cursive";
+screenWidth.style.fontSize = "20px";
+screenWidth.style.fontWeight = "800";
+screenWidth.style.color = "#dddddd";
+screenWidth.innerText = window.screen.width;
